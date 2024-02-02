47d9ae088236eabb7f1cf1e23ded2264173c5a8a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-02 09:05:42 +0100

    chore: created an interface for cart schma definition

--- models/cartModel.ts
+++ models/cartModel.ts
@@ -1,43 +1,54 @@
-const mongoose = require('mongoose');
+// My biggest task yet
 
-const cartSchema = new mongoose.Schema({
+import mongoose, { Types } from "mongoose";
+
+interface Cart {
+  quantity: number;
+  total: number;
+
+  user: Types.ObjectId;
+  product: Types.ObjectId;
+  createdt: Date;
+}
+
+const cartSchema = new mongoose.Schema<Cart>({
   quantity: {
     type: Number,
-    default: 1
+    default: 1,
   },
 
   totalPrice: {
     type: Number,
-    required: [true, 'A cart must have a total price']
+    required: [true, "A cart must have a total price"],
   },
 
   user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'A cart must belong to a user']
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "A cart must belong to a user"],
   },
 
   product: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Product',
-    required: [true, 'A product must be added to cart']
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: [true, "A product must be added to cart"],
   },
 
   createdAt: {
     type: Date,
-    default: Date.now()
-  }
+    default: Date.now(),
+  },
 });
 
-cartSchema.pre(/^find/, function(next) {
+cartSchema.pre(/^find/, function (next) {
   this.populate({
-    path: 'product',
-    select: 'name price image description'
+    path: "product",
+    select: "name price image description",
   });
 
   next();
 });
 
-const Cart = mongoose.model('Cart', cartSchema);
+const Cart = mongoose.model("Cart", cartSchema);
 
 module.exports = Cart;
