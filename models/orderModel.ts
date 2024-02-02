f3850233f415771a6effd46871b11d83c5859d3a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-02 09:32:48 +0100

    chore: specified types for Order Model

--- models/orderModel.ts
+++ models/orderModel.ts
@@ -1,48 +1,65 @@
-const mongoose = require('mongoose');
+import mongoose, { Document, Types } from "mongoose";
+
+interface IProduct {
+  productId: mongoose.Schema.Types.ObjectId;
+
+  price: number;
+  quantity: number;
+}
+
+interface IOrder extends Document {
+  createdAt: Date;
+
+  products: Types.DocumentArray<IProduct>;
+
+  totalPrice: number;
+  user: mongoose.Schema.Types.ObjectId;
+  paid: boolean;
+}
 
 const orderSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
   },
 
   products: [
     {
       productId: {
         type: mongoose.Schema.ObjectId,
-        ref: 'Product',
-        required: [true, 'Order must have a product']
+        ref: "Product",
+        required: [true, "Order must have a product"],
       },
 
       price: {
         type: Number,
-        required: [true, 'A product must have a price']
+        required: [true, "A product must have a price"],
       },
 
       quantity: {
         type: Number,
-        default: 1
-      }
-    }
+        default: 1,
+      },
+    },
   ],
 
   totalPrice: {
     type: Number,
-    required: [true, 'An order must have a total price']
+    required: [true, "An order must have a total price"],
   },
 
   user: {
     type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'Order must belog to a user']
+    ref: "User",
+    required: [true, "Order must belog to a user"],
   },
 
   paid: {
     type: Boolean,
-    default: true
-  }
+    default: true,
+  },
 });
 
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 
-module.exports = Order;
+export default Order;
